c1a797ad5321a4a5509d659e164b42b4831310d9
trevithj <trevithj@gmail.com>
2019-08-22 13:21:05 +1200

    archived old firefly app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import React from 'react';
-import LeftPanel from './leftPanel';
-import MainPanel from './mainPanel';
 import Main from './equipMe';
-import { Col, Row } from './styled';
-
-const appStyle = {
-  fontFamily: 'Open Sans',
-  margin: 0,
-  padding: 0,
-  width: '100%'
-};
 
 const App = () => {
   return <Main />;
-};
-
-export const AppOld = () => {
-  return (
-    <div className='container' style={appStyle}>
-      <Row style={{ width: '100%' }}>
-        <Col width='3'>
-          <LeftPanel />
-        </Col>
-        <Col width='9'>
-          <MainPanel />
-        </Col>
-      </Row>
-    </div>
-  );
 };
 
 export default App;
